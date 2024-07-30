72dd36fa618c1eafa8b6c0e34c8ac85d5fbe880d
Dev Single <89737813+DevSingle@users.noreply.github.com>
2024-07-30 07:17:25 -0700

    Upload Files 🚀

---
+++ Nisam-Discord-Routing/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Nisam_Discord_Routing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(470, 253);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(198, 56);
+            this.flowLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.Click += new System.EventHandler(this.FlowLayoutPanel1_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(-5, 253);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(198, 56);
+            this.flowLayoutPanel2.TabIndex = 1;
+            this.flowLayoutPanel2.Click += new System.EventHandler(this.FlowLayoutPanel2_Click);
+            this.flowLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.FlowLayoutPanel2_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Nisam_Discord_Routing.Properties.Resources._5522;
+            this.ClientSize = new System.Drawing.Size(663, 303);
+            this.ControlBox = false;
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "نیسم تیم";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+    }
+}
+
---
+++ Nisam-Discord-Routing/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Principal;
+using System.Diagnostics;
+
+namespace Nisam_Discord_Routing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FlowLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void FlowLayoutPanel2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void FlowLayoutPanel1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("درحال ست کردن روتینگ", "نیسم تیم", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            try
+            {
+                // Ensure the application is running with administrative privileges
+                if (!IsRunAsAdmin())
+                {
+                    // Restart the application with administrative privileges
+                    MessageBox.Show("برنامه باید بصورت ادمین اجرا شود!", "نیسم تیم", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    RestartAsAdmin();
+                }
+                else
+                {
+                    ModifyHostsFile();
+                    MessageBox.Show("تنظیمات روتینگ انجام شد", "نیسم تیم", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void ModifyHostsFile()
+        {
+            string hostsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), @"drivers\etc\hosts");
+            string newEntry = "162.159.134.234 gateway.discord.gg";
+
+            var lines = File.ReadAllLines(hostsPath).ToList();
+
+            if (lines.Any(line => line.Contains(newEntry)))
+            {
+                // Remove existing entry
+                lines = lines.Where(line => !line.Contains(newEntry)).ToList();
+                MessageBox.Show("تنظیمات روتینگ دیسکورد شما به حالت اولیه بازگشت!", "نیسم تیم", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                // Add new entry
+                lines.Add(newEntry);
+                MessageBox.Show("روتینگ دیسکورد با موفقیت انجام شد", "نیسم تیم", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            File.WriteAllLines(hostsPath, lines);
+        }
+
+        private bool IsRunAsAdmin()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(id);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private void RestartAsAdmin()
+        {
+            var exeName = Process.GetCurrentProcess().MainModule.FileName;
+            ProcessStartInfo startInfo = new ProcessStartInfo(exeName)
+            {
+                Verb = "runas"
+            };
+            Process.Start(startInfo);
+            Application.Exit();
+        }
+    }
+}
